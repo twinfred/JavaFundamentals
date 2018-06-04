77526d294c4f733c03761c34e379d69a7565f09b
Timothy Winfred <twinfred@gmail.com>
2018-06-04 11:50:17 -0700

    first JavaFundamentals commit

---
+++ FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Tim Winfred");
+        System.out.println("I am 28 years old");
+        System.out.println("My hometown is Tuolumne, CA");
+    }
+}
